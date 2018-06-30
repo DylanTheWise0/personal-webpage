2d72bb73a27bb58caa0fd2c271c2a0a540733d83
Dylan Wise <dylan.wise0@gmail.com>
2018-06-30 19:54:30 -0400

    got rid of unnecessary dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -16,7 +15,6 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
