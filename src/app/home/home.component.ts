c9cc2bdef801500478be117860af9ee45ae426c2
Dylan Wise <dylan.wise0@gmail.com>
2018-06-30 19:50:40 -0400

    changed reative header to be mobile friendly

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,20 +7,39 @@ import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular
 })
 export class HomeComponent implements OnInit {
 
+  public isCollapsed = true;
+
   @ViewChild('headerElement') headerElement: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    if (window.innerWidth < 992) {
+      this.headerElement.nativeElement.classList.remove('navbar-dark');
+      this.headerElement.nativeElement.classList.add('navbar-light');
+    }
   }
 
   @HostListener('window:scroll', ['$event'])  onWindowScroll($event) {
     const pageY = window.pageYOffset;
-    if (pageY > 150) {
-      this.headerElement.nativeElement.classList.add('small-header');
+    if (window.innerWidth >= 992) {
+      if (pageY > 150) {
+        this.headerElement.nativeElement.classList.add('small-header');
+        this.headerElement.nativeElement.classList.remove('navbar-dark');
+        this.headerElement.nativeElement.classList.add('navbar-light');
+      } else {
+        this.headerElement.nativeElement.classList.remove('small-header');
+        this.headerElement.nativeElement.classList.add('navbar-dark');
+        this.headerElement.nativeElement.classList.remove('navbar-light');
+      }
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])  onResize($event) {
+    if ($event.target.innerWidth < 992) {
       this.headerElement.nativeElement.classList.remove('navbar-dark');
       this.headerElement.nativeElement.classList.add('navbar-light');
-    } else {
+    } else if ($event.target.innerWidth >= 992 && window.pageYOffset <= 150) {
       this.headerElement.nativeElement.classList.remove('small-header');
       this.headerElement.nativeElement.classList.add('navbar-dark');
       this.headerElement.nativeElement.classList.remove('navbar-light');
