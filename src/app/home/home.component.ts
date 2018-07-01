13eb5b11f9bee9be5815561f979f63e4326d4554
Dylan Wise <dylan.wise0@gmail.com>
2018-07-01 12:07:34 -0400

    added about section to webpage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   public isCollapsed = true;
 
   @ViewChild('headerElement') headerElement: ElementRef;
+  @ViewChild('introductionSectionElement') introductionSectionElement: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    // if the window is small start with the light theme
     if (window.innerWidth < 992) {
       this.headerElement.nativeElement.classList.remove('navbar-dark');
       this.headerElement.nativeElement.classList.add('navbar-light');
     }
   }
 
+  ngAfterViewInit() {
+    // make introduction element fit the window upon initial load
+    this.introductionSectionElement.nativeElement.style.height = window.innerHeight + 'px';
+  }
+
   @HostListener('window:scroll', ['$event'])  onWindowScroll($event) {
+    // dynamically change header on scroll only if window isn't 'small'
     const pageY = window.pageYOffset;
     if (window.innerWidth >= 992) {
       if (pageY > 150) {
@@ -36,6 +44,7 @@ export class HomeComponent implements OnInit {
   }
 
   @HostListener('window:resize', ['$event'])  onResize($event) {
+    // make header correct format on window resize based on width
     if ($event.target.innerWidth < 992) {
       this.headerElement.nativeElement.classList.remove('navbar-dark');
       this.headerElement.nativeElement.classList.add('navbar-light');
